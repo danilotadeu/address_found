517a0e60a1e1d1c36c0377b7e4a06bd51e9b2263
Danilo Tadeu Oliveira Silva <danilotadeuoliveirasilva@MacBook-Air-de-Danilo.local>
2022-03-31 19:06:24 -0300

    melhorias no codigo

--- app/codeinformation/codeinformation.go
+++ app/codeinformation/codeinformation.go
@@ -40,18 +40,7 @@ func NewApp(urlProvider, portProvider string) App {
 
 //GetCodeInformation get a information code in webservice...
 func (a appImpl) GetCodeInformation(ctx context.Context, requestCodeInformation *codeinformationModel.CodeInformationRequest) (interface{}, error) {
-	requestbody := codeinformationModel.CustomerRetrieveRequest{}
-	requestbody.Header.Security.UsernameToken.Username = "ADMX"
-	requestbody.Header.Security.UsernameToken.Password.Type = requestCodeInformation.Password
-	if val, err := strconv.Atoi(requestCodeInformation.Customer.ID); err == nil {
-		requestbody.Body.CustomerRetrieveRequest.InputAttributes.CustomerRead.CsId = val
-		requestbody.Body.CustomerRetrieveRequest.InputAttributes.PaymentArrangementsRead.CsId = val
-		requestbody.Body.CustomerRetrieveRequest.InputAttributes.AddressesRead.CsId = val
-		requestbody.Body.CustomerRetrieveRequest.InputAttributes.CustomerInfoRead.CsId = val
-	}
-	requestbody.Body.CustomerRetrieveRequest.InputAttributes.CustomerRead.SyncWithDb = requestCodeInformation.Customer.SyncFlag
-	requestbody.Body.CustomerRetrieveRequest.SessionChangeRequest.Values.Item.Key = requestCodeInformation.UserID
-
+	requestbody := prepareBody(requestCodeInformation)
 	body, err := xml.Marshal(requestbody)
 	if err != nil {
 		log.Println("app.codeinformation.codeinformation.codeinformation.xml_marshal", err.Error())
@@ -59,7 +48,7 @@ func (a appImpl) GetCodeInformation(ctx context.Context, requestCodeInformation 
 	}
 
 	url := fmt.Sprintf("http://%s:%s/v1/r-customer-code-information-service", a.urlProvider, a.portProvider)
-	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(string(body)))
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, strings.NewReader(string(body)))
 
 	req.Header.Add("Content-Type", "text/xml")
 	if err != nil {
@@ -69,7 +58,6 @@ func (a appImpl) GetCodeInformation(ctx context.Context, requestCodeInformation 
 
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
-	req = req.WithContext(ctx)
 	client := http.DefaultClient
 
 	/* LOG INICIO */
@@ -78,7 +66,11 @@ func (a appImpl) GetCodeInformation(ctx context.Context, requestCodeInformation 
 	log.Println("    INFO    EngDB | Engineering | GMID |  | - | ENGNB003153 | rCustomerCodeInformationService | 1 | - | - | " + formatted + " | - | - | [rCustomerCodeInformationService] |  input: {'msisdn':'123'}")
 	timeDur := 5 * time.Second
 	log.Println("    DEBUG    EngDB | Engineering | GMID |  | - | ENGNB003153 | rCustomerCodeInformationService | 1 | - | - | " + formatted + " | - | - | Http Provider request | TIMEOUT[" + strconv.Itoa(int(timeDur.Seconds())) + "] METHOD[POST] URL[" + url + "]")
-	payload, _ := json.Marshal(requestbody)
+	payload, err := json.Marshal(requestbody)
+	if err != nil {
+		log.Println("app.codeinformation.codeinformation.codeinformation.json_marshal_payload", err.Error())
+		return nil, err
+	}
 	log.Println("    DEBUG    EngDB | Engineering | GMID |  | - | ENGNB003153 | rCustomerCodeInformationService | 1 | - | - | " + formatted + " | - | - | request body | " + string(payload))
 	log.Println("    DEBUG    EngDB | Engineering | GMID |  | - | ENGNB003153 | rCustomerCodeInformationService | 1 | - | - | " + formatted + " | - | - | provider header | [" + contentType + ", " + contentTypeValue + "]")
 	/* LOG FIM */
@@ -111,11 +103,30 @@ func (a appImpl) GetCodeInformation(ctx context.Context, requestCodeInformation 
 
 	var responseService codeinformationModel.CodeInformationResponseProviderError
 	err = xml.Unmarshal(data, &responseService)
-	responseProviderJSON, _ := json.Marshal(responseService)
+	responseProviderJSON, err := json.Marshal(responseService)
+	if err != nil {
+		log.Println("app.codeinformation.codeinformation.codeinformation.json_marshal_response_provider_json", err.Error())
+		return nil, err
+	}
 	log.Println("    DEBUG    EngDB | Engineering | GMID |  | - | ENGNB003153 | rCustomerCodeInformationService | 1 | - | - | " + formatted + " | - | - | out | " + string(responseProviderJSON))
 	if err != nil {
 		log.Println("app.codeinformation.codeinformation.codeinformation.xml_unmarshal_provider_error", err.Error())
 		return nil, err
 	}
 	return &responseService, nil
+}
+
+func prepareBody(data *codeinformationModel.CodeInformationRequest) codeinformationModel.CustomerRetrieveRequest {
+	requestbody := codeinformationModel.CustomerRetrieveRequest{}
+	requestbody.Header.Security.UsernameToken.Username = "ADMX"
+	requestbody.Header.Security.UsernameToken.Password.Type = data.Password
+	if val, err := strconv.Atoi(data.Customer.ID); err == nil {
+		requestbody.Body.CustomerRetrieveRequest.InputAttributes.CustomerRead.CsId = val
+		requestbody.Body.CustomerRetrieveRequest.InputAttributes.PaymentArrangementsRead.CsId = val
+		requestbody.Body.CustomerRetrieveRequest.InputAttributes.AddressesRead.CsId = val
+		requestbody.Body.CustomerRetrieveRequest.InputAttributes.CustomerInfoRead.CsId = val
+	}
+	requestbody.Body.CustomerRetrieveRequest.InputAttributes.CustomerRead.SyncWithDb = data.Customer.SyncFlag
+	requestbody.Body.CustomerRetrieveRequest.SessionChangeRequest.Values.Item.Key = data.UserID
+	return requestbody
 }
